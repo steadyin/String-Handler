8288eefa42a946a6a97a5f3d3a29c1cd6ddc7747
JIhoon-Park <steadyin@naver.com>
2022-05-16 11:24:05 +0900

    setting:컨트롤러, 서비스 세팅

---
+++ src/main/java/com/steadyin/stringhandler/controll/ParseController.java
@@ -0,0 +1,24 @@
+package com.steadyin.stringhandler.controll;
+
+import com.steadyin.stringhandler.dto.ParseRequest;
+import com.steadyin.stringhandler.dto.ParseResponse;
+import com.steadyin.stringhandler.service.ParseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class ParseController {
+    private final ParseService parseService;
+
+  @PostMapping("api/parse")
+  public ResponseEntity<ParseResponse> parse(@RequestBody @Valid ParseRequest request) {
+      final ParseResponse response = parseService.parse(request);
+      return ResponseEntity.ok(response);
+  }
+}
