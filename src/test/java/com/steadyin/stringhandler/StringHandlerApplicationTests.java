d0a8d964a0a559ea9e01aac646619af2ef8de716
JIhoon-Park <steadyin@naver.com>
2022-05-16 11:20:08 +0900

    setting:프로젝트 세팅

---
+++ src/test/java/com/steadyin/stringhandler/StringHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steadyin.stringhandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringHandlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
